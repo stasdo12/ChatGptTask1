20420c2b8d46984aadeade9c0f48362be0e4ffef
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-06 14:23:25 +0200

    created sql file to create table in db

---
+++ src/main/resources/db/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todos (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       description TEXT NOT NULL
+);
